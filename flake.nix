6e440ec852c68966f4f5496238754ec612994d3d
paradoxical-dev <jaredonnell21@gmail.com>
2026-05-01 15:31:26 -0500

    feat(flake): added initial host and flake framework

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Wubba lubba dub dub";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "nixpkgs/nixos-25.11";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+
+    let
+      lib = nixpkgs.lib;
+      hosts = builtins.filter (x: x != null) (
+	lib.mapAttrsToList (
+	  k: v: if (v == "directory") then k else null
+	) (builtins.readDir ./hosts)
+      );
+    in {
+	nixosConfigurations = builtins.listToAttrs (
+	  map (host: {
+	    name = host;
+	    value = lib.nixosSystem {
+
+	      modules = [
+	        { config.networking.hostName = host; }
+		(./hosts + "/${host}")
+
+		home-manager.nixosModules.home-manager
+		{
+		  home-manager.useGlobalPkgs = true;
+		  home-manager.useUserPackages = true;
+		  home-manager.extraSpecialArgs = {
+		    inherit inputs;
+		  };
+		}
+	      ];
+
+	      specialArgs = { inherit inputs; };
+	    };
+	  }) hosts
+	);
+    };
+}
