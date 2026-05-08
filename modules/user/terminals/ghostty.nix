22c172d993f712335e645506e80af8f262f078ca
paradoxical-dev <jaredonnell21@gmail.com>
2026-05-07 23:11:13 -0500

    feat(terminals): added ghostty custom theme(s)

--- modules/user/terminals/ghostty.nix
+++ modules/user/terminals/ghostty.nix
@@ -6,6 +6,7 @@
 
 let
   cfg = config.userSettings.ghostty;
+  themePath = "${config.userSettings.dotfilesDir}/modules/user/terminals/ghostty/themes";
 in
 {
   options = {
@@ -22,11 +23,11 @@ in
       font-family-italic = "Victor Mono";
       font-family-bold-italic = "Victor Mono";
 
-      theme = "Nord";
+      theme = "${themePath}/Nordic";
 
       window-decoration = "none";
-      window-padding-x = 5;
-      window-padding-y = 5;
+      # window-padding-x = 5;
+      # window-padding-y = 5;
       window-padding-balance = true;
     };
   };
