a5b52d4c03ec0ca4ed830d0a75da43e7154c8a59
paradoxical-dev <jaredonnell21@gmail.com>
2026-05-16 19:50:29 -0500

    feat(ghostty): added cursor shaders

--- modules/user/terminals/ghostty.nix
+++ modules/user/terminals/ghostty.nix
@@ -1,11 +1,18 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 
 let
   cfg = config.userSettings.ghostty;
+  ghosttyShaders = pkgs.fetchFromGitHub {
+    owner = "sahaj-b";
+    repo = "ghostty-cursor-shaders";
+    rev = "main";
+    hash = "sha256-G/UIr1bKnxn1AcHl/4FL/jou6b7M2VeREslYVELxdmw=";
+  };
   themePath = "${config.userSettings.dotfilesDir}/modules/user/terminals/ghostty/themes";
 in
 {
@@ -15,6 +22,12 @@ in
     };
   };
   config = lib.mkIf cfg.enable {
+    # copy over shaders
+    home.file.".config/ghostty/shaders" = {
+      source = ghosttyShaders;
+      recursive = true;
+    };
+
     programs.ghostty.enable = true;
     programs.ghostty.systemd.enable = true;
     programs.ghostty.settings = {
@@ -24,6 +37,7 @@ in
       font-family-bold-italic = "Victor Mono";
 
       theme = "${config.home.homeDirectory}/.config/ghostty/themes/current";
+      custom-shader = "${config.home.homeDirectory}/.config/ghostty/shaders/cursor_warp.glsl";
 
       window-decoration = "none";
       window-padding-balance = true;
